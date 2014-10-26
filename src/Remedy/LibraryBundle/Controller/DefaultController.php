be7b3fe720ae9283a9261f9c3e26f2d73c85d8f6
Hilary Barr <hbarr@remedypartners.com>
2014-10-26 13:32:31 -0400

    Delete works

--- src/Remedy/LibraryBundle/Controller/DefaultController.php
+++ src/Remedy/LibraryBundle/Controller/DefaultController.php
@@ -12,12 +12,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use JMS\Serializer\Annotation\AccessType;
-
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 
 
 use Remedy\LibraryBundle\Document\Book;
@@ -29,36 +23,6 @@ class DefaultController extends Controller
 
 {
 
-//NEED TO SPECIFY POST METHOD
-    /**
-     * @Route("/create")
-     *
-     */
-    public function createAction(Request $request)
-
-    {
-        $in = json_decode($request->getContent());
-
-
-        $book = new Book();
-        $book->title = ($in->title);
-        $book->author=($in->author);
-        $book->price=($in->price);
-        $book->quantity=($in->quantity);
-
-        $dm = $this->get('doctrine_mongodb')->getManager();
-        $dm->persist($book);
-        $dm->flush();
-
-
-        $response = new Response;
-        $response->setContent($request->getContent());
-        $response->headers->set("Access-Control-Allow-Origin", "*");
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-
-
-    }
 
     /**
      * @Route("/show")
@@ -79,97 +43,67 @@ class DefaultController extends Controller
 
 
 
+//NEED TO SPECIFY POST METHOD
+    /**
+     * @Route("/create")
+     *
+     */
+    public function createAction(Request $request)
 
-
-
-
-
-
-
-    public function updateAction($id)
     {
+        $in = json_decode($request->getContent());
+
+
+        $book = new Book();
+        $book->title = ($in->title);
+        $book->author = ($in->author);
+        $book->price = ($in->price);
+        $book->quantity = ($in->quantity);
+
         $dm = $this->get('doctrine_mongodb')->getManager();
-        $book = $dm->getRepository('RemedyLibraryBundle:Book')->find($id);
-
-
-        $dm->remove($product);
+        $dm->persist($book);
         $dm->flush();
 
-        return $this->redirect($this->generateUrl('show'));
+
+        $response = new Response;
+        $response->setContent($request->getContent());
+        $response->headers->set("Access-Control-Allow-Origin", "*");
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+
+
     }
 
 
+
+//
+    /**
+     * @Route("/delete/{id}")
+     */
     public function deleteAction($id)
     {
         $dm = $this->get('doctrine_mongodb')->getManager();
         $book = $dm->getRepository('RemedyLibraryBundle:Book')->find($id);
 
-
-        $book->setTitle('New book title!');
+        $dm->remove($book);
         $dm->flush();
-
-        return $this->redirect($this->generateUrl('show'));
     }
 
 
-    /**
-     * @Route("/list")
-     * @Template()
-     * @return JsonResponse
-     */
-    public function listAction()
-
-    {
-        $books = array(
-            ['title' => 'JavaScript for Dummies', 'author' => 'Remedy Partners', 'price' => 1.50, 'quantity' => 2],
-            ['title' => 'The Raven', 'author' => 'Edgar Allan Poe', 'price' => 3.76, 'quantity' => 5],
-            ['title' => 'PHP is Awesome', 'author' => 'Max', 'price' => 4.00, 'quantity' => 1],
-            ['title' => 'Home', 'author' => 'Jodi Picoult', 'price' => 3.76, 'quantity' => 10],
-            ['title' => 'The Scarlet Letter', 'author' => 'Nathaniel Hawthorne', 'price' => 6.00, 'quantity' => 1]
-        );
-        $response = new Response;
-        $response->setContent(json_encode($books));
-        $response->headers->set("Access-Control-Allow-Origin", "*");
-        $response->headers->set('Content-Type', 'application/json');
-        return $response;
-
-    }
+//
+//
+//
+//
+//    public function updateAction($id)
+//    {
+//        $dm = $this->get('doctrine_mongodb')->getManager();
+//        $book = $dm->getRepository('RemedyLibraryBundle:Book')->find($id);
+//
+//
+//        $dm->remove($product);
+//        $dm->flush();
+//
+//        return $this->redirect($this->generateUrl('show'));
+//    }
 
 }
-
-// ALTERNATIVE
-///**
-// * @Route("/list")
-// * @Template()
-// * @return JsonResponse
-// */
-//public function listAction()
-//
-//{
-//    $request->request->get('json')
-//
-//        return new Response(json_encode(array('dataReceived' => $data));
-//    //return rendered HTML page:
-//     return $this->render('RemedyBundle:Default:list.html.twig', array(
-//         'books' => $data));
-//
-//    }
-//
-//var books = { title : 'Book1', author: 'Me' };
-//
-//$.ajax({
-//  type: "POST",
-//  url: url,//post how you get this URL please...
-//  data: {json : books},
-//  success: function(response)
-//  {
-//      console.log(response);
-//  }
-//  error: function()
-//{
-//    console.log('an error occured');
-//    console.log(arguments);//get debugging!
-//}
-//});
-//
-//
